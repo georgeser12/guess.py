26c5abf60df48eca77b3f8f77a0e9e0bd4fed537
georgeser12 <63621163+georgeser12@users.noreply.github.com>
2021-02-10 09:38:59 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,46 +1,57 @@
 import random
 
 
+#picking random number
 secretNumber = random.randint(1, 10)
 
 
+#play again feature
 def play_again():
     print('play again (yes or no): ')
     return input('').lower().startswith('y')
 
+
+#getting player guess
 def get_guess():
     global secretNumber
+    
+    #variables
     winner = True
     guesses = 5
     print("range: (1-10)")
     numbers = []
+    #loop to get guesses
     while winner:
         try:
             guess = int(input('Guess a number: '))
 
-
+            #make sure if they got the right number
             if guess == secretNumber:
                 print(f'YES! the secret number was {secretNumber}')
                 winner = False
-
+                
+            #checking if they already guessed that number
             elif guess in numbers:
                 print("you already guessed this number")
 
+             #making sure the guess is within range
             elif guess not in range(1,10):
                 print('that number was not within the range')
 
+            # if they get the number wrong, you lose tries
             elif guess != secretNumber:
                 guesses -= 1
                 print('Nope incorrect')
                 numbers.append(guess)
 
-
+            #run out of guesses, game ends
             if guesses == 0:
                 print(f'sorry you ran out of tries, the answer was {secretNumber}')
                 winner = False
+        # expecting Valueerrors and returning a 'invalid response' instead of crashing
         except ValueError:
             print('invalid response')
-
+    # if game ends ask if they'd like to play again
     if winner == False:
         if play_again():
             winner = True
